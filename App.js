34f78f5dd05d10a4ea84b52cde60b0cced7099d8
bedirhan <celayirbedirhan@gmail.com>
2023-03-06 01:57:01 +0300

    chore: run project

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // This is the entry point if you run `yarn expo:start`
 // If you run `yarn ios` or `yarn android`, it'll use ./index.js instead.
-import App from "./app/app.tsx"
+import App from "./samuraiContacts/app/app.jsx"
 import React from "react"
 import { registerRootComponent } from "expo"
 import * as SplashScreen from "expo-splash-screen"
