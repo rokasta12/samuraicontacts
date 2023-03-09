3fcdcbd184ace5a9c3053dbb3adcba9749a3da17
bedirhan <celayirbedirhan@gmail.com>
2023-03-10 01:32:50 +0300

    fix: import error and init w yarn

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // This is the entry point if you run `yarn expo:start`
 // If you run `yarn ios` or `yarn android`, it'll use ./index.js instead.
-import App from "./samuraiContacts/app/app.jsx"
+import App from "./app/app.tsx"
 import React from "react"
 import { registerRootComponent } from "expo"
 import * as SplashScreen from "expo-splash-screen"
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // If you use Expo (`yarn expo:start`), the entry point is ./App.js instead.
 // Both do essentially the same thing.
 
-import App from "./samuraiContacts/app/app.jsx"
+import App from "./app/app.tsx"
 import React from "react"
 import { AppRegistry } from "react-native"
 import RNBootSplash from "react-native-bootsplash"
